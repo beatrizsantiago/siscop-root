3f71a6d5b533618206f63b3f0fcf3aa6ad6cc9df
Beatriz Santiago <beatrizsantiago1999@hotmail.com>
2025-05-24 15:57:55 -0300

    ✨ feat: update TypeScript configuration and Vite aliases for improved module resolution

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,10 @@ export default defineConfig({
     alias: {
       '@assets': path.resolve(__dirname, 'src/assets'),
       '@components': path.resolve(__dirname, 'src/components'),
+      '@domain': path.resolve(__dirname, 'src/domain'),
+      '@infrastructure': path.resolve(__dirname, 'src/infrastructure'),
+      '@usecases': path.resolve(__dirname, 'src/usecases'),
+      '@generalTypes': path.resolve(__dirname, 'src/types'),
     },
   },
   optimizeDeps: {

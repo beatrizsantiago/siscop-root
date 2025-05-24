d1c1174b328862ae304dce394b9ce82c2e3df154
Beatriz Santiago <beatrizsantiago1999@hotmail.com>
2025-05-24 15:57:59 -0300

    ✨ feat: add ToastContainer to App component for improved user notifications

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -2,13 +2,15 @@ import { theme } from 'agro-core';
 import { ThemeProvider } from "@mui/material/styles";
 import { CssBaseline } from '@mui/material';
 import { RouterProvider } from 'react-router';
-import router from './router';
+import { ToastContainer } from 'react-toastify';
 
+import router from './router';
 
 const App = () => (
   <ThemeProvider theme={theme}>
     <CssBaseline />
     <RouterProvider router={router} />
+    <ToastContainer />
   </ThemeProvider>
 );
 

b43cc34f87bf27d467096e45e350fd8d814c5df1
Beatriz Santiago <beatrizsantiago1999@hotmail.com>
2025-06-11 20:58:25 -0300

    ✨ feat: implement lazy loading of modules in ProtectedRoute and add loading indicator

---
+++ src/components/PreloadModules.tsx
@@ -0,0 +1,6 @@
+import('products/products-app');
+import('farms/farms-app');
+import('inventory/inventory-app');
+import('sales/sales-app');
+import('production/production-app');
+import('goals/goals-app');
--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,9 +1,25 @@
-import { Navigate, Outlet } from 'react-router';
+import { useEffect } from 'react';
+import { Navigate, Outlet, useNavigation } from 'react-router';
 import { localStorageService } from '@infrastructure/services/localStorage';
+import { CircularProgress } from '@mui/material';
 
 const ProtectedRoute = () => {
+  const navigation = useNavigation();
+
   const token = localStorageService.getToken();
-  return token ? <Outlet /> : <Navigate to="/" replace />;
+
+  useEffect(() => {
+    import('@components/PreloadModules');
+  }, []);
+
+  return token
+    ? (
+      <>
+        {navigation.state === 'loading' && <CircularProgress />}
+        <Outlet />
+      </>
+    )
+    : <Navigate to="/" replace />;
 };
 
 export default ProtectedRoute;
